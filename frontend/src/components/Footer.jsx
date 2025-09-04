79497a4f763d044976ecee6bd98e6839bb0a14e8
nova054 <ajaya.kuikel054@gmail.com>
2025-09-04 10:31:03 +0545

    Next commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -88,7 +88,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400 text-sm">
-            © 2024 VolunteerMe. All rights reserved.
+            © 2025 VolunteerMe. All rights reserved.
           </p>
         </div>
       </div>
